15e3131e13c16c0d6ed9545466be2a1c529f98a8
nikhilvaibhav <nikhilvaibhav545@gmail.com>
2022-06-20 11:26:15 +0530

    ternary rendering

--- App.tsx
+++ App.tsx
@@ -7,19 +7,33 @@ export default function App() {
     setbtn((prevState) => !prevState);
   };
 
-  if (btn) {
-    return (
-      <div>
-        <h2>Welcome Nikhil, Hope you having a good time</h2>
-        <button onClick={action}>LogOut</button>
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <h2>Welcomo Guest !!!</h2>
-        <button onClick={action}>LogIn</button>
-      </div>
-    );
-  }
+  //ternary operator way of conditional rendering
+  return btn ? (
+    <div>
+      <h2>Hello Nikhil</h2>
+      <button onClick={action}>LogOut</button>
+    </div>
+  ) : (
+    <div>
+      <h2>Welcome Guest</h2>
+      <button onClick={action}>LogIn</button>
+    </div>
+  );
+
+  //if-else way if conditional rendering
+  // if (btn) {
+  //   return (
+  //     <div>
+  //       <h2>Welcome Nikhil, Hope you having a good time</h2>
+  //       <button onClick={action}>LogOut</button>
+  //     </div>
+  //   );
+  // } else {
+  //   return (
+  //     <div>
+  //       <h2>Welcomo Guest !!!</h2>
+  //       <button onClick={action}>LogIn</button>
+  //     </div>
+  //   );
+  // }
 }
